cb421b09cce2587654dc4b3b209f5b2fad17583f
Joe Lutovsky <joe.lutovsky@gmail.com>
2023-01-20 18:34:04 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const acc = document.getElementsByClassName("btn-letter");
+const example = document.getElementsByClassName("example");
+
 const backButton = document.querySelectorAll(".btn-back");
 
 //Refactor this using forEach sometime
@@ -12,6 +14,18 @@ for (let i = 0; i < acc.length; i++) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+for (let i = 0; i < example.length; i++) {
+  example[i].addEventListener("click", function () {
+    this.classList.toggle("active-example");
+    const panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "1px";
     }
   });
 }
